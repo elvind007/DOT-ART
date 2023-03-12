deeea3a1180935fa4681bea62b445c266a602be0
Elvin Dsouza <emdd007@gmail.com>
2023-03-12 23:21:32 +0530

    done

--- ascii.py
+++ ascii.py
@@ -1,106 +1,32 @@
-from sys import argv
-from PIL import Image, UnidentifiedImageError
+from PIL import Image
+
+ASCII_CHARS = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@', '$', 'S', 'H', 'M', 'W']
 
 
-DEFAULT_IMAGE = "data/fish.jpeg"
-
-# Output text file.
-ASCII_FILE_PATH = "ascii_image.txt"
-
-# Width (row size) of the output text.
-# The higher this value is, the more details the ASCII text will show.
-# Set this to 0 to keep the original size.
-OUTPUT_WIDTH = 120
-
-# ASCII characters to replace the image pixels with. Sorted to match from lowest
-# to highest level of brightness.
-SEQUENCE = " .:-=+*#%@"
-
-# When True, map "higher value" characters of SEQUENCE to the shadowy part of
-# the image.
-# To be used when displaying black text on white background.
-PRODUCE_NEGATIVE = False
-
-
-
-def resize_image(image, new_width):
-    """Resize the given image to have the specified new width and a new height
-    that mantains the original aspect ratio.
-
-    If new_width param is 0 the image will NOT be modified."""
-    if new_width == 0:
-        return image
-    old_width, old_height = image.size
-    new_height = int( old_height * new_width / old_width * 0.55 )
-    return image.resize((new_width, new_height), Image.ANTIALIAS)
-
-
-def negative(ascii_string):
-    """Replace the characters of the ASCII string to generate a negative
-    representation of the image."""
-    new_string = ""
-    for c in ascii_string:
-        if c == "\n":
-            new_string += "\n"
-        else:
-            index = len(SEQUENCE) - SEQUENCE.index(c) - 1
-            new_string += SEQUENCE[index]
-    return new_string
-
-
-def create_ascii_string(image):
-    """Find, for each pixel of the image, the corresponding character of the
-    ASCII sequence to form a string."""
-    string = ""
+def resize_image(image, new_width=100):
     width, height = image.size
-    pixels = image.load()
+    aspect_ratio = height / width
+    new_height = int(new_width * aspect_ratio)
+    return image.resize((new_width, new_height))
 
-    for y in range(height):
-        for x in range(width):
-            pixel = pixels[x, y]
-            # Get index of character in SEQUENCE corresponding to current
-            # pixel's brightness.
-            index = int( pixel / 255 * (len(SEQUENCE) - 1) )
-            # Add character to the string.
-            string += SEQUENCE[index]
-        # New line for every new row of pixels.
-        string += "\n"
-    return string
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    ascii_pixels = ''.join([ASCII_CHARS[pixel//25] for pixel in pixels])
+    return ascii_pixels
 
 
+def image_to_ascii(image_path, output_path):
 
-if __name__ == "__main__":
-    if len(argv) > 1:
-        image_path = argv[1]
-    else:
-        if DEFAULT_IMAGE:
-            image_path = DEFAULT_IMAGE
-        else:
-            print("No image given.")
-            exit(1)
+    image = Image.open(image_path)
+    image = resize_image(image)
 
-    # Open the image file.
-    try:
-        image = Image.open(image_path)
-    except FileNotFoundError:
-        print(f"Image file '{image_path}' not found.")
-        exit(1)
-    except UnidentifiedImageError:
-        print(f"File '{image_path}' is not an image.")
-        exit(1)
+    image = image.convert('L')
 
-    # Resize image to have a width of 80 characters while keeping the original
-    # aspect ratio
-    image = resize_image(image, OUTPUT_WIDTH)
 
-    # Turn image to greyscale to get, for each pixel its brightness value.
-    image = image.convert("L")
+    ascii_pixels = pixels_to_ascii(image)
+    lines = [ascii_pixels[index:index+image.width] for index in range(0, len(ascii_pixels), image.width)]
 
-    # Map pixels to characters.
-    ascii_text = create_ascii_string(image)
-    if PRODUCE_NEGATIVE:
-        ascii_text = negative(ascii_text)
+    with open(output_path, 'w') as f:
+        f.write('\n'.join(lines))
 
-    # Write string to output file.
-    with open(ASCII_FILE_PATH, "w", encoding="UTF-8") as file:
-        file.write(ascii_text)
+image_to_ascii('data/1.jpeg', 'data/output.txt')
--- readme.md
+++ readme.md
@@ -1,3 +1,1 @@
-
-This program takes an image and produces a copy of it made entirely of [ASCII]
-printable characters; this copy can, therefore, be stored in a plain text file.
+This program takes an image and produces a copy of it made entirely of [ASCII] printable characters; stored in a plain text file.
